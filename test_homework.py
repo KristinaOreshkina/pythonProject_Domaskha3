9133fd910cfe2ee76b1bda6bdf050991e1b3f7d3
Kristina Oreshkina <tinaore@mail.ru>
2025-10-16 16:11:55 +0300

    добавлен код

--- test_homework.py
+++ test_homework.py
@@ -1,3 +1,5 @@
+from cmath import pi
+from random import randint
 
 
 def test_greeting():
@@ -25,39 +27,41 @@ def test_rectangle():
     a = 10
     b = 20
     # TODO сосчитайте периметр
-    perimeter = 0
+    perimeter = 2*(a+b)
+
+    assert perimeter == 60
+
 
     assert perimeter == 60
 
     # TODO сосчитайте площадь
-    area = 0
+    area = a*b
 
     assert area == 200
 
 
 def test_circle():
     """
-    Напишите программу, которая берет радиус круга и выводит на экран его длину и площадь.
-    Используйте константу PI
-    """
+       Напишите программу, которая берет радиус круга и выводит на экран его длину и площадь.
+       Используйте константу PI
+       """
     r = 23
     # TODO сосчитайте площадь
-    area = 0
+    area = r**2 * pi
 
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
-    length = 0
+    length = 2 * pi * r
 
     assert length == 144.51326206513048
-
 
 def test_random_list():
     """
     Создайте список из 10 случайных чисел от 1 до 100 (включая обе границы) и отсортируйте его по возрастанию.
     """
     # TODO создайте список
-    l = []
+    l = sorted([randint(1, 100) for i in range(10)])
 
     assert len(l) == 10
     assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
@@ -69,7 +73,7 @@ def test_unique_elements():
     """
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
     # TODO удалите повторяющиеся элементы
-
+    l = list(set(l))
     assert isinstance(l, list)
     assert len(l) == 10
     assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -84,7 +88,7 @@ def test_dicts():
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
-    d = {}
+    d =  dict(zip(first, second))
 
     assert isinstance(d, dict)
     assert len(d) == 5
